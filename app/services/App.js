4aa2b799a36724072a5f4e59177dd4e6851b1fa3
Kabir <kabeer182010@gmail.com>
2015-03-12 10:43:09 +0545

    Added App service

---
+++ app/services/App.js
@@ -0,0 +1,17 @@
+(function() {
+
+    var app = angular.module('notesApp');
+
+    var apiBase = 'http://127.0.0.1:8080/';
+
+    app.factory('App', function($http) {
+
+        return {
+            apiUrl: getApiUrl
+        };
+
+        function getApiUrl(path) {
+            return apiBase + (path || '');
+        }
+    });
+})();
--- app/services/Notes.js
+++ app/services/Notes.js
@@ -2,7 +2,47 @@
 
     var app = angular.module('notesApp');
 
-    app.factory('Notes', function($http) {
+    app.factory('Notes', function($http, App) {
 
+        return {
+            all: getAll,
+            create: create,
+            get: get,
+            update: update,
+            destroy: destroy
+        };
+
+        function getAll() {
+            var url = App.apiUrl('notes');
+
+            return $http.get(url);
+        }
+
+        function get(id) {
+            var url = App.apiUrl('notes/' + id);
+
+            return $http.get(url);
+        }
+
+        function create(data) {
+
+            var url = App.apiUrl('notes/');
+
+            return $http.post(url, data);
+        }
+
+        function update(id, data) {
+
+            var url = App.apiUrl('notes/' + id);
+
+            return $http.put(url, data);
+
+        }
+
+        function destroy(id) {
+            var url = App.apiUrl('notes/' + id);
+
+            return $http.delete(url);
+        }
     });
 })();
